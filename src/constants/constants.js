413d6e56d059679159e62acaf83f360f59a520f6
ishikaa-2302 <75898672+ishikaa-2302@users.noreply.github.com>
2021-09-14 04:24:25 +0530

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -20,8 +20,8 @@ export const projects = [
   {
     title: 'Stock Rates',
     description: "A fully responsive dynamic webpage which takes the data from Json format and displays itself in a graph. It is built with different features like searching and filtering to dynamically view the data in the graph and table",
-      image: '/images/3.jpg',
-      tags: ['PHP', 'SQL','Data Visualisation'],
+    image: '/images/3.jpg',
+    tags: ['PHP', 'SQL','Data Visualisation'],
     source: 'https://github.com/ishikaa-2302/Stock',
     visit: 'https://github.com/ishikaa-2302/Stock',
     id: 2,
@@ -29,11 +29,29 @@ export const projects = [
   {
     title: 'Calcluator',
     description: "A fully responsive calculator with neumorphism effect",
-      image: '/images/3.png',
-      tags: ['JS', 'Bootstrap','Design'],
+    image: '/images/3.png',
+    tags: ['JS', 'Bootstrap','Design'],
     source: 'https://github.com/ishikaa-2302/Calculator',
-    visit: 'https://github.com/ishikaa-2302/Calculator',
-    id: 2,
+    visit: 'https://basic-calculator-ui.netlify.app/',
+    id: 3,
+  },
+  {
+    title: 'Online CV',
+    description: "A fully responsive static document-like webpage where you can display your resume in web.",
+    image: '/images/4.png',
+    tags: ['Bootstrap'],
+    source: 'https://github.com/ishikaa-2302/Online-CV',
+    visit: 'https://online-resume-doc.netlify.app/',
+    id: 4,
+  }
+  {
+    title: 'To-do List',
+    description: "A fully-responsive simple to-do list that lets you add/delete/update the tasks. It is a full stack application which uses MERN Stack to perform CRUD(Create,Read,Update and Delete) Operations.",
+    image: '/images/interface.png',
+    tags: ['MongoDb','Express','React','NodeJs'],
+    source: 'https://github.com/ishikaa-2302/todo-list',
+    visit: 'https://github.com/ishikaa-2302/todo-list',
+    id: 5
   }
 ];
 
